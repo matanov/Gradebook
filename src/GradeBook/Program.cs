adff75678aa1caa5c9b66147caee9937fde626eb
Panteley Matanov <matanov@hotmail.com>
2020-06-08 15:23:51 +0000

    Commit online

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0){
+            Console.WriteLine($"Hello World! Panto is Here {args[0]}");
+            }
+
+            else{
+                Console.WriteLine("No user input");
+            }
+        }
+        
+    }
+}
