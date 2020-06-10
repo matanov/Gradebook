3e386b3e65cc84a2a72e530477316f533006da93
Panteley Matanov <matanov@hotmail.com>
2020-06-10 02:53:36 +0000

    Updated ShowsStatsitcs method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+ using System;
  using System.Collections.Generic;
  
  namespace GradeBook
@@ -5,9 +6,10 @@
  
     class Book
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
             
 
@@ -15,7 +17,26 @@
         {
             grades.Add(grade);
         }
+
+        public void ShowStatistics()
+        {
+            var average =0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var grade in grades){
+                average += grade;
+                highGrade = Math.Max(highGrade,grade);
+                lowGrade = Math.Min(lowGrade,grade);
+            }
+            average /= grades.Count;
+            System.Console.WriteLine($"Average grade is {average:N1}");
+            System.Console.WriteLine($"Highest garde is {highGrade}");
+            System.Console.WriteLine($"Lowest grade is {lowGrade}");
+
+        }
         List<double> grades;
+        private string name;
     }
 
  }                                  
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,21 +9,30 @@ namespace GradeBook
         static void Main(string[] args)
         {
 
-        var book = new Book();
+        var book = new Book("Scott's grae book");
         book.AddGrade(89.1);
         book.AddGrade(90.5);
+        book.AddGrade(77.5);
+        book.ShowStatistics();
 
-        //var grades =  new List<double>() {12.7, 10.3,6.11,4.1};
-       // grades.Add(56.1);
-    
-        //var result =0.0;
-        //foreach (var number in grades)
-        //{
-         //   result += number;
-       // }
-        //result /= grades.Count;
+    //    var grades =  new List<double>() {12.7, 10.3,6.11,4.1};
+    //    grades.Add(56.1);
+    //    var result =0.0;
+    //    var highGrade = double.MinValue;
+    //    var lowestGrade = double.MaxValue;
+    //    foreach (var number in grades)
+    //    {
+    //        highGrade = Math.Max(number, highGrade);
+    //        lowestGrade = Math.Min(number, lowestGrade);
+    //        result += number;
+    //    }
+    //    result /= grades.Count;
         
-        //Console.WriteLine($"The average is {result:N2}");
+    //     Console.WriteLine($"The lowest grade is {lowestGrade}");
+    //     Console.WriteLine($"The highest grade is {highGrade}");
+        
+    //     Console.WriteLine($"The average is {result:N2}");
+
         }
 
     }
