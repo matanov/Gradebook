17e6bf176eb8c91fa04ad247ec6bb9fed8a1a84a
Panteley Matanov <matanov@hotmail.com>
2020-06-10 02:54:02 +0000

    Updated Main

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -15,23 +15,7 @@ namespace GradeBook
         book.AddGrade(77.5);
         book.ShowStatistics();
 
-    //    var grades =  new List<double>() {12.7, 10.3,6.11,4.1};
-    //    grades.Add(56.1);
-    //    var result =0.0;
-    //    var highGrade = double.MinValue;
-    //    var lowestGrade = double.MaxValue;
-    //    foreach (var number in grades)
-    //    {
-    //        highGrade = Math.Max(number, highGrade);
-    //        lowestGrade = Math.Min(number, lowestGrade);
-    //        result += number;
-    //    }
-    //    result /= grades.Count;
-        
-    //     Console.WriteLine($"The lowest grade is {lowestGrade}");
-    //     Console.WriteLine($"The highest grade is {highGrade}");
-        
-    //     Console.WriteLine($"The average is {result:N2}");
+
 
         }
 
